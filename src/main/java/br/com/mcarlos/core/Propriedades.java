99a8891c1a507ec8ab24a5c477f0b01da37cd5a7
Mayrlon Carlos <mayrloncarlos@gmail.com>
2019-08-09 07:39:36 -0300

    Implementacao dos arquivos base para o teste

---
+++ src/main/java/br/com/mcarlos/core/Propriedades.java
@@ -0,0 +1,13 @@
+package br.com.mcarlos.core;
+
+public class Propriedades {
+
+	public static boolean FECHAR_BROWSER = false;
+	
+	public static Browsers browser = Browsers.CHROME;
+	
+	public enum Browsers {
+		CHROME,
+		FIREFOX
+	}
+}
